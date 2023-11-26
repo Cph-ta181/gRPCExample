caf0c025ba41e8d0e8f0bce1d8949382cb2be270
Thomas <cph-ta181@cphbusiness.dk>
2023-11-26 14:08:50 +0100

    initial

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using gRPCServer;
+
+string name = Console.ReadLine();
+var input = new HelloRequest { Name = name };
+
+var channel = GrpcChannel.ForAddress("https://localhost:7071");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(input);
+
+Console.WriteLine(reply.Message);
+
+
+Console.WriteLine("----Customers----\nType what ID you would like to look up:");
+string idInput = Console.ReadLine();
+var input2 = new CustomerLookupModel { UserId = Int32.Parse(idInput) };
+var client2 = new Customer.CustomerClient(channel);
+var reply2 = await client2.GetCustomerInfoAsync(input2);
+Console.WriteLine(reply2);
+
+Console.ReadLine();
---
+++ gRPCServer/Services/CustomersService.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+
+namespace gRPCServer.Services
+{
+	public class CustomersService : Customer.CustomerBase
+	{
+		private readonly ILogger<CustomersService> logger;
+
+		public CustomersService(ILogger<CustomersService> logger)
+		{
+			this.logger = logger;
+		}
+
+		public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+		{
+			CustomerModel output = new CustomerModel();
+			if (request.UserId == 0)
+			{
+				output.FirstName = "Bob";
+				output.LastName = "Bobsen";
+			}
+			else
+			{
+				output.FirstName = "Ib";
+				output.LastName = "Ibsen";
+			}
+
+			return Task.FromResult(output);
+		}
+
+	}
+}
